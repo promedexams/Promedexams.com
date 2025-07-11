db4625d042c4fae1a8d7363693dcde4caf41b43c
Dylan Ravel <dylanm.ravel@gmail.com>
2025-07-11 00:17:01 -0600

    Fixed services page

--- app/[lang]/(appointments)/schedule-appointment/page.tsx
+++ app/[lang]/(appointments)/schedule-appointment/page.tsx
@@ -1,14 +1,14 @@
 import Footer from "@/components/footer/footer";
 import Navbar from "@/components/navbar/navbar";
 
-const ScheduleAppointmentPage = () => {
+const ScheduleAppointmentPage = async ({ params }: { params: Promise<{ lang: "en" | "es" }> }) => {
   return (
     <div className="relative w-screen min-h-screen flex flex-col p-0 m-0 bg-[#4F759B]">
-      <Navbar />
+      <Navbar params={params} />
       <main className="md:relative flex flex-col gap-2 flex-1 justify-center items-center space-y-2 p-4 z-10 bg-[#4F759B]">
         <h1>Schedule Appointment Page</h1>
       </main>
-      <Footer />
+      <Footer params={params} />
     </div>
   );
 };
